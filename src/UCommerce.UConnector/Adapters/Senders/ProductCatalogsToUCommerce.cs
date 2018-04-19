5a041b347993103184ad32eecc2a83ec2f60c89c
Albert Hickman <kingfishercatchfire@gmail.com>
2018-04-19 13:13:17 -0400

    temporary fix for missing sitefinityid definition field

--- src/UCommerce.UConnector/Adapters/Senders/ProductCatalogsToUCommerce.cs
+++ src/UCommerce.UConnector/Adapters/Senders/ProductCatalogsToUCommerce.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using MigrationCommon.Exceptions;
 using NHibernate;
+using NHibernate.Linq;
 using uCommerce.uConnector.Helpers;
 using UCommerce.EntitiesV2;
 using UConnector.Framework;
@@ -27,12 +28,50 @@ namespace uCommerce.uConnector.Adapters.Senders
             {
                 WritePriceGroups(catalog);
                 WriteCatalog(catalog);
+                WriteCustomSitefinityIdDefinitionField();
             }
             catch (Exception ex)
             {
                 Log.Fatal($"A fatal exception occurred trying to write catalog data to Ucommerce: \n{ex}");
                 throw new MigrationException("A fatal exception occurred trying to write catalog data to Ucommerce", ex);
             }
+        }
+
+        /// <summary>
+        /// HACK/TODO: This was quickly added after some third party testing discovered that the
+        ///       'SitefinityId' DefinitionField is not getting added on the fly in the Taxonomy RTS
+        ///       flow.  Refactor this out of here, either making a separate RTS operation/flow for 
+        ///       custom property definitions or figure out why UCommerce/nHibernate is not 
+        ///       creating new Definition Fields on the fly as part of cascade saves in the Taxonomy flow. 
+        /// </summary>
+        private void WriteCustomSitefinityIdDefinitionField()
+        {
+            const string sitefinityUniqueIdPropertyName = "SitefinityId";
+            var definitionField = _session.Query<DefinitionField>().FirstOrDefault(x => x.Name == sitefinityUniqueIdPropertyName);
+
+            if (definitionField != null) return;
+
+            using (var tx = _session.BeginTransaction())
+            {
+                var defaultDefinition = _session.Query<Definition>()
+                    .FirstOrDefault(x => x.Name == "Default Category Definition");
+                var dataType = _session.Query<DataType>().FirstOrDefault(x => x.TypeName == "ShortText");
+                definitionField = new DefinitionField()
+                {
+                    Name = sitefinityUniqueIdPropertyName,
+                    Multilingual = false,
+                    Searchable = false,
+                    BuiltIn = false,
+                    DefaultValue = string.Empty,
+                    DisplayOnSite = false,
+                    DataType = dataType,
+                    Definition = defaultDefinition
+                };
+                _session.SaveOrUpdate(definitionField);
+                tx.Commit();
+            }
+
+            _session.Flush();
         }
 
         private void WritePriceGroups(ProductCatalog catalog)
